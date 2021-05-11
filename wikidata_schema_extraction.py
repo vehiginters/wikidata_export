3772909695d89681f20a1e14c07cb8cd50e578f3
vehiginters <vehiginters@gmail.com>
2021-05-11 18:14:49 +0300

    Major changes, most importantly getting class relations

--- wikidata_schema_extraction.py
+++ wikidata_schema_extraction.py
@@ -5,6 +5,20 @@ from configparser import ConfigParser
 import psycopg2 #Dependency used for connection to postgreSql database
 import time
 
+#List used for keeping track of number of queries in the last minute
+LAST_MINUTE_EVENTS = list()
+
+def countPastQueries():
+    # Function used to go over the last event list and cut off the list after finding the first expired event
+    global LAST_MINUTE_EVENTS
+    TIME_WINDOW = 60
+    tim=time.time()  # called only once
+    for idx in range(len(LAST_MINUTE_EVENTS)-1,-1,-1):
+        if LAST_MINUTE_EVENTS[idx]+TIME_WINDOW<= tim:
+            LAST_MINUTE_EVENTS[:idx+1]=""
+            break
+    return len(LAST_MINUTE_EVENTS)
+
 def config(section, filename='properties.ini'):
     parser = ConfigParser()
     parser.read(filename)
@@ -31,39 +45,54 @@ def getDbCon(params):
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
-def queryWikiData(query):
+def queryWikiData(query, retries=0):
+    global LAST_MINUTE_EVENTS
     # Make POST request to wikidata sparsql service
+    lastMinuteEvents = countPastQueries()
+    if lastMinuteEvents > 10:
+        # If more than 10 queries been made in the last minute wait for a while, not to fail
+        print("More than 10 queries in the last minute, waiting for 60s")
+        time.sleep(61)
+    if retries == 3:
+        # TODO - Currently there is a problem, that a failing query even if it was because of too many requests,
+        # the failing query will go into an endless fail loop
+        print("Bad requests loop skipping query for now")
+        return {}
     url = 'https://query.wikidata.org/sparql'
     body = {'query': query,
             'format': 'json',
+            # Proper user-agent to identify the caller as specified by WikiData query API specification
             'User-Agent': 'Wikidata schema extraction Bot/1.0 (https://github.com/vehiginters/wikidata_export, vehiginters@gmail.com)',}
+    LAST_MINUTE_EVENTS.append(time.time())
     response = requests.post(url, data = body)
     if response.ok:
-        return json.loads(response.text)
-    elif response.status_code == 429: # To many requests in the last minute, let's wait some time till we make a new one
-        sleepTime = 30
-        if response.headers["Retry-After"]:
+        return json.loads(response.text)['results']['bindings']
+    elif response.status_code == 429 or response.status_code == 503 : # To many requests in the last minute, let's wait some time till we make a new one
+        sleepTime = 60
+        if "Retry-After" in response.headers:
             sleepTime = int(response.headers["Retry-After"])
         print("Query Limit reached. Retrying after {}s".format(sleepTime))
-        time.sleep(sleepTime)
-        queryWikiData(query)
+        time.sleep(sleepTime+1)
+        queryWikiData(query+" ", retries+1)
     elif response.status_code == 502: # Bad gateway server, let's just retry the query
         print("Got bad gateway server in response, retrying query...")
-        time.sleep(10)
-        queryWikiData(query)
+        time.sleep(30)
+        queryWikiData(query+" ", retries+1)
     else:
         print("WikiData returned response code - {}".format(response.status_code))
 
 def insertClasses(connection, dict):
     # Insert classes from given dictionary into target database
     cur = connection.cursor()
-    baseSql = "INSERT INTO sample.classes(iri, cnt, display_name) VALUES('{}', {}, '{}');\n"
+    # Subclasses used while developing, just to see how many subclasses for relevant classes are there
+    baseSql = "INSERT INTO sample.classes(iri, cnt, display_name, local_name, is_unique, subclasses) VALUES('{}', {}, '{}', '{}', true, '{}');\n"
     totalSql = ""
     for key, value in dict.items():
         labelValue = value['label']
         if "'" in labelValue:
+            # " ' " needs to be escaped for postgresql 
             labelValue = labelValue.replace("'", "''")
-        totalSql = totalSql + baseSql.format(key,value['subclasses'], labelValue)
+        totalSql = totalSql + baseSql.format(key,value['instances'], labelValue, value['localname'], value['subclasses'])
     cur.execute(totalSql)
     connection.commit()
     cur.close()
@@ -74,13 +103,58 @@ def insertProperties(connection, dict):
     baseSql = "INSERT INTO sample.properties(iri, cnt, display_name) VALUES('{}', {}, '{}');\n"
     totalSql = ""
     for key, value in dict.items():
+        useCount = value['useCount']
+        if useCount > 2100000000:
+            # There is one property with more than 2'100'000'000, which goes out of properties table 'cnt' column integer range, so just put it at limit
+            useCount = 2100000000
         labelValue = value['label']
         if "'" in labelValue:
+            # Same as for classes " ' " needs to be escaped for postgresql
             labelValue = labelValue.replace("'", "''")
-        totalSql = totalSql + baseSql.format(key,value['useCount'], labelValue)
+        totalSql = totalSql + baseSql.format(key, useCount, labelValue)
     cur.execute(totalSql)
     connection.commit()
     cur.close()
+
+def insertClassPropertyRelations(connection, dict):
+    baseSql = '''
+    INSERT INTO sample.cp_rels(class_id, property_id, type_id) 
+    SELECT (SELECT id from sample.classes WHERE iri = '{classIri}') AS cl_id,
+    (SELECT id from sample.properties WHERE iri = '{propIri}') AS pr_id,
+    1
+    HAVING (SELECT id from sample.classes WHERE iri = '{classIri}') IS NOT NULL
+    AND (SELECT id from sample.properties WHERE iri = '{propIri}') IS NOT NULL;
+    '''
+    # To-Do the hardcoded type id should be replaces like select from type table, just like for class-class relations
+    relations = len(dict)
+    totalSql = ""
+    i = 0
+    for key, value in dict.items():
+        i = i + 1
+        totalSql = totalSql + baseSql.format(classIri = value['classId'], propIri = value['propId'])
+        if ((i % 1000) == 0) or (i == relations):
+            cur.execute(totalSql)
+            totalSql = ""
+    connection.commit()
+    cur.close()
+
+def insertClassClassRelations(cursor, relationList):
+    print("Inserting class relations into target database")
+    # As the Python script has no idea about IDs of the classes, just tell the SQL to select them based on class iri's
+    # Should watch out, as the iri technically could not be unique, as that could brake this SQL
+    baseSql = '''
+        INSERT INTO sample.cc_rels(class_1_id, class_2_id, type_id)
+        SELECT (SELECT id from sample.classes WHERE iri = '{class1Iri}') AS cl_id,
+        (SELECT id from sample.classes WHERE iri = '{class2Iri}') AS pr_id,
+        (SELECT id from sample.cc_rel_types WHERE name = 'sub_class_of')
+        HAVING (SELECT id from sample.classes WHERE iri = '{class1Iri}') IS NOT NULL
+        AND (SELECT id from sample.classes WHERE iri = '{class2Iri}') IS NOT NULL;
+    '''
+    totalSql = ""
+    for class1, class2  in relationList:
+        totalSql = totalSql + baseSql.format(class1Iri = class1, class2Iri = class2)
+    cursor.execute(totalSql)
+    # Don't commit transaction just yet, because these relations are inserted in batches and not all at once
 
 def getProperties():
     print("Getting list of properties...")
@@ -93,8 +167,9 @@ def getProperties():
     """
     responseDict = queryWikiData(query)
     resultDict = {}
-    for i in responseDict['results']['bindings']:
-        resultDict[i['property']['value']] = {'useCount':i['useCount']['value'], 'label': ""}
+    if responseDict is not None:
+        for i in responseDict:
+            resultDict[i['property']['value']] = {'useCount':int(i['useCount']['value']), 'label': ""}
     return resultDict
 
 def getPropertyLabels(propertiesDict):
@@ -117,43 +192,113 @@ def getPropertyLabels(propertiesDict):
         # Can't query in much bigger batches as then queries start to reach payload limit
         if ((i % 15000) == 0) or (i == totalProps):
             responseDict = queryWikiData(query.format(propertyList))
-            for j in responseDict['results']['bindings']:
-                if j['property']['value'] in propertiesDict:
-                    propertiesDict[j['property']['value']]['label'] = j['propLabel']['value']
+            if responseDict is not None:
+                for j in responseDict:
+                    if j['property']['value'] in propertiesDict:
+                        propertiesDict[j['property']['value']]['label'] = j['propLabel']['value']
             propertyList = ""
-            print("{} done...".format(i))
+            print("{:.1%} done...".format(i/float(totalProps)))
+
+def getClassClassRelations(connection, classDict):
+    print("Getting Class-Class relations...")
+    # A little complicated function, that gets all subclass relations between all relevant classes
+    query = """
+        select distinct ?class ?subclass where {{
+          ?subclass wdt:P279 ?class.
+          VALUES ?class {{ {} }}
+        }}
+    """
+    i = 0
+    relationsCounter = 0
+    totalClasses = len(classDict)
+    classList = ""
+    relationList = []
+    collectedClasses = 0
+    cur = connection.cursor()
+    totalInsertedRelations = 0
+    # Iteration logic: for first iteration just take the class values and continue, for rest first check if to many subclasses collected and only after query collect the current class
+    # This is done so that we can kinda look at the number of subclasses for next class in dictionary
+    for key, value in classDict.items():
+        i = i + 1
+        if i == 1 or i == totalClasses:
+            collectedClasses = collectedClasses + 1
+            classList = classList + " <" + key + ">"
+            relationsCounter = relationsCounter + int(value['subclasses'])
+            if i == 1:
+                continue
+        # First check if either the max number of subclasses for collected classes go over 1mil, a total of 15000 classes is collected or if its the last class
+        if ((relationsCounter + int(value['subclasses'])) > 1000000) or (collectedClasses  == 15000) or (i == totalClasses):
+            # If either check is true, query wikidata to get related classes for batch of the collected classes
+            responseDict = queryWikiData(query.format(classList))
+            # It was a bit heavy to check if the subclass is relevant on wikidata, so it is done within Python
+            # But then again we get max 1mil of result rows in response, which can take up to 1GB RAM for a few secs, while the response is processed
+            if responseDict is not None:
+                for j in responseDict:
+                    if j['subclass']['value'] in classDict:
+                        relationList.append((j['class']['value'], j['subclass']['value']))
+            responseDict.clear() # Clear the response dict as fast as we can, to free up used memory
+            classList = ""
+            relationsCounter = 0
+            collectedClasses = 0
+            currentRelations = len(relationList)
+            print("Relations for {}/{} classes done...".format(i, totalClasses))
+            # After we collect more then 50k relations, or we are at the end, insert the class relations, but dont commit the transaction yet
+            if (currentRelations > 50000) or (i == totalClasses):
+                insertClassClassRelations(cur, relationList)
+                totalInsertedRelations = totalInsertedRelations + currentRelations
+                print("{} relations collected".format(totalInsertedRelations))
+                relationList.clear()
+        collectedClasses = collectedClasses + 1
+        classList = classList + " <" + key + ">"
+        relationsCounter = relationsCounter + int(value['subclasses'])
+    connection.commit()
+    cur.close()
+
+def getClassPropertyRelations():
+    # To-Do
+    query = """
+        select distinct ?property ?class where {{
+          ?y ?property ?class.
+          ?y wdt:P279|wdt:P31 ?class.
+          VALUES ?property {{ {} }}
+        }}
+    """
 
 def getClasses():
-    # Get all of the relevant classes from WikiData with at least 1 instance or subclass
-    # First get the classes with subclasses
-    print("Getting list of subclass classes...")
-    query = """
-        select ?class (count(?y) as ?subclasses) where {{
-           ?y wdt:P279 ?class
-        }}
-        GROUP BY ?class
-        ORDER BY DESC(?subclasses)
-    """
-    responseDict = queryWikiData(query)
-    resultDict = {}
-    for i in responseDict['results']['bindings']:
-        resultDict[i['class']['value']] = {'subclasses':i['subclasses']['value'], 'label': ""}
-    # Then get the classes with instances
-    print("Getting list of instance classes...")
+    # Get all of the relevant classes from WikiData with at least 1 instance
+    # First get the classes with their instance count
+    print("Getting list classes...")
     query = """
         select ?class (count(?y) as ?instances) where {{
-          ?y wdt:P31 ?class
+           ?y wdt:P31 ?class.
         }}
         GROUP BY ?class
         ORDER BY DESC(?instances)
     """
     responseDict = queryWikiData(query)
-    for i in responseDict['results']['bindings']:
-        if i['class']['value'] in resultDict:
-            resultDict[i['class']['value']]['subclasses'] = resultDict[i['class']['value']]['subclasses'] + i['instances']['value']
-        else:
-            resultDict[i['class']['value']] = {'subclasses':i['instances']['value'], 'label': ""}
-    return resultDict
+    classDict = {}
+    if responseDict is not None:
+        for i in responseDict:
+            localName = ""
+            if i['class']['value'][:31] == "http://www.wikidata.org/entity/":
+                localName = i['class']['value'][31:]
+            classDict[i['class']['value']] = {'instances':int(i['instances']['value']), 'label': "", 'subclasses': 0, 'localname': localName}
+    print("{} classes retrieved".format(len(classDict)))
+    # Then count the number of subclasses for each class, later used for getting class relations
+    print("Counting class subclasses...")
+    query = """
+        select ?class (count(?y) as ?subclasses) where {{
+           ?y wdt:P279 ?class.
+        }}
+        GROUP BY ?class
+        ORDER BY DESC(?subclasses)
+    """
+    responseDict = queryWikiData(query)
+    if responseDict is not None:
+        for i in responseDict:
+            if i['class']['value'] in classDict:
+                classDict[i['class']['value']]['subclasses'] = int(i['subclasses']['value'])
+    return classDict
 
 def getClassLabels(classDict):
     # Get labels for classes in a given dictionary
@@ -173,22 +318,27 @@ def getClassLabels(classDict):
         # Query wikidata in batches of 15000 to maximize query time and minimize amount of queries
         if ((i % 15000) == 0) or (i == totalClasses):
             responseDict = queryWikiData(query.format(classList))
-            for j in responseDict['results']['bindings']:
-                if j['class']['value'] in classDict:
-                    classDict[j['class']['value']]['label'] = j['classLabel']['value']
+            if responseDict is not None:
+                for j in responseDict:
+                    if j['class']['value'] in classDict:
+                        classDict[j['class']['value']]['label'] = j['classLabel']['value']
             classList = ""
-            print("{} done...".format(i))
+            print("{:.1%} done...".format(i/float(totalClasses)))
 
 if __name__ == '__main__': 
     conf = config('postgreSqlConnection')
-    
+
     getDbCon(conf)
+
     propDict = getProperties()
     getPropertyLabels(propDict)
     insertProperties(DB_CON, propDict)
     propDict.clear() # Clear the massive dictionary, to not take up RAM space
+
     classDict = getClasses()
     getClassLabels(classDict)
     insertClasses(DB_CON, classDict)
+    getClassClassRelations(DB_CON, classDict)
     classDict.clear()
+
     DB_CON.close()
